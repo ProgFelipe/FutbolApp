190312c818ff75d6dc443ec44b39143662524d16
Johan <johanda-777@hotmail.com>
2014-09-18 15:17:15 -0500

    ultimo login

--- app/src/main/java/example/futbolapp/Login.java
+++ app/src/main/java/example/futbolapp/Login.java
@@ -1,19 +1,54 @@
 package example.futbolapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Login extends Activity {
+
+    private String user;
+    private String passw;
+    private String persona = "David";
+    private String contrasenia = "12345";
+    private TextView aux;
+    private TextView aux1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        TextView usuario = (TextView) findViewById(R.id.usuario);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        aux = usuario;
+        aux1 = password;
+
     }
 
+    public void login(View view)
+    {
+        user = String.valueOf(aux.getText());
+        passw = String.valueOf(aux1.getText());
+
+        if(user.equals(persona) && passw.equals(contrasenia))
+        {
+            Intent intent = new Intent(this, mainActivity.class);
+            startActivity(intent);
+        }else
+        {
+            Toast.makeText(getApplicationContext(),"Verifique los datos", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
