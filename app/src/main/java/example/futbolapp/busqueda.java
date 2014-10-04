ba261c0042c03836ebc9b3040ca48d5f185e87ac
felipegi91 <felipegi91@gmail.com>
2014-10-04 11:20:42 -0500

    Obtencion del Json y  visualizacion en el layout.

--- app/src/main/java/example/futbolapp/busqueda.java
+++ app/src/main/java/example/futbolapp/busqueda.java
@@ -3,12 +3,26 @@ package example.futbolapp;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
-
+import android.widget.TextView;
+import android.widget.Toast;
+//Json libraries
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import example.futbolapp.database.MergeData;
 import example.futbolapp.database.local.DB_Manager;
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxStatus;
+
+import java.util.ArrayList;
 
 /**
  * Created by Usuario on 11/09/2014.
@@ -18,10 +32,32 @@ public class busqueda extends ActionBarActivity {
     private DB_Manager manager;
     private ListView lista;
     SimpleCursorAdapter adapter;
+    //AQuery object
+    AQuery aq;
+
+    ListView listView;
+
+    SearchView searchView ;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.busqueda);
+        //Instantiate AQuery Object
+        aq = new AQuery(this);
+        listView = (ListView)findViewById(R.id.listViewSearch);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(aq.getContext(), "Go to see field info and option to register", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+        searchView = (SearchView) findViewById(R.id.searchView);
+        String SearchedField = searchView.toString();
+        //Update fields with JSON response
+        getFields();
+
         //SQLite
         //Creación o retorno de la base de datoS
         /*DB_Manager manager = new DB_Manager(this);
@@ -42,7 +78,8 @@ public class busqueda extends ActionBarActivity {
                 "6.256145"," -75.588220","","Canchas de futbol el Marte 2");*/
         //Get all fields from the web service
         //And put them in this layout
-        new MergeData();
+        //new MergeData();
+
 
 
        /* manager = new DB_Manager(this);
@@ -58,6 +95,52 @@ public class busqueda extends ActionBarActivity {
         //gridView.setAdapter(new ImageAdapter(this));
 
     }
+    public void getFields(){
+        //JSON URL
+        String url = "http://solweb.co/reservas/api/field/fields";
+        //Make Asynchronous call using AJAX method and show progress gif until get info
+        aq.progress(R.id.progressBarSearch).ajax(url, JSONObject.class, this,"jsonCallback");
+    }
 
-
+    public void jsonCallback(String url, JSONObject json, AjaxStatus status) {
+        //When JSON is not null
+        Log.v("JSON", json.toString());
+        if (json != null) {
+            ArrayList<String> fields = new ArrayList<String>();
+            String jsonResponse = "";
+            try {
+                //Get json as Array
+                JSONArray jsonArray = json.getJSONArray("field");
+                if (jsonArray != null) {
+                    int len = jsonArray.length();
+                    for (int i = 0; i < len; i++) {
+                        //Get the name of the field from array index
+                        fields.add(jsonArray.getJSONObject(i).getString("name"));
+                    }
+                }
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                Toast.makeText(aq.getContext(), "Error in parsing JSON", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(aq.getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_dropdown_item_1line, fields);
+            listView.setAdapter(adapter);
+        }
+        //When JSON is null
+        else {
+            //When response code is 500 (Internal Server Error)
+            if(status.getCode() == 500){
+                Toast.makeText(aq.getContext(),"Server is busy or down. Try again!",Toast.LENGTH_SHORT).show();
+            }
+            //When response code is 404 (Not found)
+            else if(status.getCode() == 404){
+                Toast.makeText(aq.getContext(),"Resource not found!",Toast.LENGTH_SHORT).show();
+            }
+            //When response code is other 500 or 404
+            else{
+                Toast.makeText(aq.getContext(),"Unexpected Error occured",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
