469f1581cf3ad277f76e85862416348ce116ac4e
Johan <johanda-777@hotmail.com>
2014-10-07 18:55:59 -0500

    commit login fb

--- app/src/main/java/example/futbolapp/Login.java
+++ app/src/main/java/example/futbolapp/Login.java
@@ -3,6 +3,7 @@ package example.futbolapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,10 +11,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.widget.LoginButton;
+
 import org.w3c.dom.Text;
 
-
-public class Login extends Activity {
+public class Login extends FragmentActivity {
 
     private String user;
     private String passw;
@@ -21,6 +23,8 @@ public class Login extends Activity {
     private String contrasenia = "12345";
     private TextView aux;
     private TextView aux1;
+    private MainFragment mainFragment;
+
 
 
     @Override
@@ -28,13 +32,25 @@ public class Login extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+
         //Button btnLogin = (Button) findViewById(R.id.btnLogin);
         TextView usuario = (TextView) findViewById(R.id.usuario);
         TextView password = (TextView) findViewById(R.id.password);
 
         aux = usuario;
         aux1 = password;
-
     }
 
     public void login(View view)
