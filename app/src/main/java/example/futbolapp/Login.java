50da691b8e2d4d46779b5cbe2900d0cd2a93291c
Johan <johanda-777@hotmail.com>
2014-10-08 15:46:15 -0500

    ultima actualizacion de login

--- app/src/main/java/example/futbolapp/Login.java
+++ app/src/main/java/example/futbolapp/Login.java
@@ -2,18 +2,28 @@ package example.futbolapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.pm.Signature;
 
+import com.facebook.android.Facebook;
 import com.facebook.widget.LoginButton;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login extends FragmentActivity {
 
@@ -23,15 +33,51 @@ public class Login extends FragmentActivity {
     private String contrasenia = "12345";
     private TextView aux;
     private TextView aux1;
-    private MainFragment mainFragment;
+    //private MainFragment mainFragment;
 
-
+    Facebook fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        String API_ID = getString(R.string.AppID);
+        fb = new Facebook(API_ID);
+
+        Button loginFb = (Button) findViewById(R.id.authButton);
+
+        loginFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fb.isSessionValid())
+                {
+                    try {
+                        fb.logout(getApplicationContext());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+        // para obtener la key hash
+        /*
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "example.futbolapp",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                System.out.println("KeyHash : "+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+        */
+        /*
         if (savedInstanceState == null) {
             // Add the fragment on initial activity setup
             mainFragment = new MainFragment();
@@ -44,6 +90,7 @@ public class Login extends FragmentActivity {
             mainFragment = (MainFragment) getSupportFragmentManager()
                     .findFragmentById(android.R.id.content);
         }
+        */
 
         //Button btnLogin = (Button) findViewById(R.id.btnLogin);
         TextView usuario = (TextView) findViewById(R.id.usuario);
