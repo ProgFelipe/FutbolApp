d2ef59fd8c3b2f5d2c58997881ad3e2e33df0d1c
felipegi91 <felipegi91@gmail.com>
2014-10-18 00:14:29 -0500

    Eventos por usuario conectado con servicio json de reservas por usuario.

--- app/src/main/java/example/futbolapp/eventsActivity.java
+++ app/src/main/java/example/futbolapp/eventsActivity.java
@@ -12,6 +12,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxStatus;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import example.futbolapp.View.DrawerItemCustomAdapter;
 import example.futbolapp.View.ObjectDrawerItem;
@@ -26,11 +35,20 @@ public class eventsActivity extends ActionBarActivity {
     ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
+    //AQuery object
+    AQuery aq;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eventos);
-
+        Bundle extras = getIntent().getExtras();
+        //getTheIdUser
+        String idUser = "5";
+        if (extras != null) {
+            idUser = extras.getString("idUser");
+        }
+        //Instantiate AQuery Object
+        aq = new AQuery(this);
         mTitle = mDrawerTitle = getTitle();
 
         mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
@@ -75,6 +93,7 @@ public class eventsActivity extends ActionBarActivity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 
+        getReservationByUser(idUser);
     }
 
     @Override
@@ -154,4 +173,64 @@ public class eventsActivity extends ActionBarActivity {
             }
 
         }
+
+
+    public void getReservationByUser(String idUser){
+        //JSON URL
+        String url = "http://solweb.co/reservas/api/reservations/byuser/"+idUser;
+        //Make Asynchronous call using AJAX method and show progress gif until get info
+        aq.progress(R.id.progressBarEvents).ajax(url, JSONObject.class, this,"jsonCallback");
+    }
+
+    public void jsonCallback(String url, JSONObject json, AjaxStatus status) {
+        //When JSON is not null
+        if (json != null) {
+            //Log.v("JSON", json.toString());
+            String jsonResponse = "";
+            try {
+                //Get json as Array
+                JSONArray jsonArray = json.getJSONArray("reservation");
+                Toast.makeText(aq.getContext(), jsonArray.toString(), Toast.LENGTH_LONG).show();
+                if (jsonArray != null) {
+                    int len = jsonArray.length();
+                    for (int i = 0; i < len; i++) {
+                        //Get the events
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        //Example
+//{"reservation": [{"id":"46","name":"carlos","hour":"1900","day":"20","month":"05","year":"2014",
+// "description":"","idField":"41","idLogin":"5"}]}
+//Elements to calendar view
+                            /*jsonObject.getString("day");
+                            jsonObject.getString("hour");
+                            jsonObject.getString("month");
+                            jsonObject.getString("year");
+                            jsonObject.getString("description");
+                            jsonObject.getString("idField");
+                            jsonObject.getString("idLogin");*/
+                    }
+                }
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                Toast.makeText(aq.getContext(), "Error in parsing JSON", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(aq.getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        }
+        //When JSON is null
+        else {
+            //When response code is 500 (Internal Server Error)
+            if(status.getCode() == 500){
+                Toast.makeText(aq.getContext(),"Server is busy or down. Try again!",Toast.LENGTH_SHORT).show();
+            }
+            //When response code is 404 (Not found)
+            else if(status.getCode() == 404){
+                Toast.makeText(aq.getContext(),"Resource not found!",Toast.LENGTH_SHORT).show();
+            }
+            //When response code is other 500 or 404
+            else{
+                Toast.makeText(aq.getContext(),"Verifique su conexion",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     }
